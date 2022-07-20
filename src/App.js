44f0971198200fc3c58351349c78551f19da8cc0
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2022-07-20 16:37:52 +0100

    nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "./styles.scss";
+import useAxiosPost from "./request";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> hello world </h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <nav classname="nav">
+          <a href="/html/"> SO FUNKTIONIERT's</a> |
+          <a href="/css/">SONDERANGEBOTE</a>
+          <select id="cars">
+            <option value="none" selected disabled hidden>
+              MEIN BEREICH
+            </option>
+            <option value="volvo">My publicshed jokes</option>
+            <option value="saab">My saved jokes</option>
+            <option value="opel">Account Informatio </option>
+            <option value="audi">Publish New Joke</option>
+          </select>
+        </nav>
       </header>
+      {/* {console.log(
+        useAxiosPost(" https://api.chucknorris.io/jokes/search?query=all")
+      )} */}
+      <h1> hello</h1>
     </div>
   );
 }
