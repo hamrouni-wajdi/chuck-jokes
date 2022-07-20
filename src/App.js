fa3bd83861130c536b7518f4b26ebcf56af2d281
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2022-07-20 15:01:19 +0100

    fixing sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.scss";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1> hello world </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
